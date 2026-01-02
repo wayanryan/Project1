4c24466720fdca1979f96e3e8c865b620baaf48f
wayanryan <134121918+wayanryan@users.noreply.github.com>
2026-01-02 11:08:29 +0800

    "-Build animasi js untuk menu - Build animasi js untuk slider "

---
+++ js/main.js
@@ -0,0 +1,196 @@
+/* SHOW MENU */
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+const tiers = document.querySelectorAll('.tier__data');
+const modal = document.getElementById('tierModal');
+const closeModal = document.getElementById('closeModal');
+
+const modalImg = document.getElementById('modalImg');
+const modalTitle = document.getElementById('modalTitle');
+const modalPrice = document.getElementById('modalPrice');
+const modalBenefits = document.getElementById('modalBenefits');
+
+
+/* MENU SHOW */
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/* MENU HIDDEN */
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/* REMOVE MENU MOBILE */
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/* HOME SWIPER */
+let homeSwiper = new Swiper(".home-swiper", {
+  spaceBetween: 30,
+  loop: "true",
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+
+// BUTTON: GO TO SLIDE 2
+document.querySelectorAll(".go-next-slide").forEach(btn => {
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    homeSwiper.slideNext();
+  });
+});
+
+/* change body's background color */
+
+let root = document.documentElement;
+
+homeSwiper.on("transitionEnd", function (e) {
+  if (this.activeIndex == 1) {
+    root.style.setProperty(
+      "--body-color",
+      "linear-gradient(to right, #D9EAFD, #FBFBFB)"
+    );
+    root.style.setProperty("--sub", "#222222");
+    root.style.setProperty("--title-color", "#222222");
+    root.style.setProperty(
+      "--container-color",
+      "linear-gradient(136deg, #D9EAFD, #FBFBFB)"
+    );
+  }
+  if (this.activeIndex == 2) {
+    root.style.setProperty(
+      "--body-color",
+      "linear-gradient(to right, #FFFFFF, #ECEEDF)"
+    );
+    root.style.setProperty("--sub", "#303056");
+    root.style.setProperty("--title-color", "#303056");
+    root.style.setProperty(
+      "--container-color",
+      "linear-gradient(136deg, #FFFFFF, #B4B4B8)"
+    );
+  }
+  if (this.activeIndex == 3) {
+    root.style.setProperty(
+      "--body-color",
+      "linear-gradient(to right, #FFFFFF, #FF9292)"
+    );
+    root.style.setProperty("--sub", "#222222");
+    root.style.setProperty("--title-color", "#222222");
+    root.style.setProperty(
+      "--container-color",
+      "linear-gradient(136deg, #FFFFFF, #FF9292)"
+    );
+  }
+});
+/* CHANGE BACKGROUND HEADER */
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+/* NEW SWIPER */
+let newSwiper = new Swiper(".new-swiper", {
+  slidesPerView: 3,
+  spaceBetween: 16,
+  loop: false
+});
+
+
+
+/* SCROLL SECTIONS ACTIVE LINK */
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+/* SHOW SCROLL UP */
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 460 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 460) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+/* SCROLL REVEAL ANIMATION */
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400
+  // reset: true
+});
+
+sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`);
+sr.reveal(`.tier__data, .trick__content, .footer__content`, {
+  interval: 100
+});
+sr.reveal(`.about__data, .discount__img`, { origin: "left" });
+sr.reveal(`.about__img, .discount__data`, { origin: "right" });
+
+//modal
+tiers.forEach(tier => {
+  tier.addEventListener('click', () => {
+    modalImg.src = tier.dataset.img;
+    modalTitle.textContent = tier.dataset.title;
+    modalPrice.textContent = tier.dataset.price;
+
+    modalBenefits.innerHTML = '';
+    tier.dataset.benefits.split(',').forEach(item => {
+      const li = document.createElement('li');
+      li.textContent = item;
+      modalBenefits.appendChild(li);
+    });
+
+    modal.classList.add('active');
+  });
+});
+
+closeModal.addEventListener('click', () => {
+  modal.classList.remove('active');
+});
+
+modal.addEventListener('click', e => {
+  if (e.target === modal) {
+    modal.classList.remove('active');
+  }
+});
